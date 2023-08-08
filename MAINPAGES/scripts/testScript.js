6157bff9b8bf8df7aed55061093cac255db747d2
Anatoly Fedorov <anakek228@gmail.com>
2023-08-08 11:09:23 +0300

    Сделал ежедневник

--- MAINPAGES/scripts/testScript.js
+++ MAINPAGES/scripts/testScript.js
@@ -2,17 +2,84 @@ const vidgetLine = document.querySelector('.vidgetLine');
 const vidgetContext = document.querySelector('.vidgetContext')
 
 vidgetLine.addEventListener("click", function(event){
-    for (var i = vidgetLine.children.length - 1; i >= 0; i--) {
-        vidgetLine.children[i].classList.remove('activeButton');
-    }
+    // for (var i = vidgetLine.children.length - 1; i >= 0; i--) {
+    //     vidgetLine.children[i].classList.remove('activeButton');
+    // }
 
     for (var i = vidgetContext.children.length - 1; i >= 0; i--) {
         vidgetContext.children[i].classList.remove('activeBlock');
     }
 
     if (event.target.closest('button')){
-        event.target.closest('button').classList.add('activeButton');
+        // event.target.closest('button').classList.add('activeButton');
+        event.target.closest('button').classList.toggle('activeButton');
 
         document.getElementById(event.target.closest('button').getAttribute('data-target')).classList.add('activeBlock');
     }
+})
+
+
+// Ежедневник
+function sortTable(columnIndex) {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("myTable");
+    switching = true;
+    
+    while (switching) {
+      switching = false;
+      rows = table.rows;
+      
+      for (i = 1; i < (rows.length - 1); i++) {
+        shouldSwitch = false;
+        x = rows[i].getElementsByTagName("td")[columnIndex];
+        y = rows[i + 1].getElementsByTagName("td")[columnIndex];
+  
+        // Для сортировки даты рождения предварительно преобразуем строку в объект Date
+        if (columnIndex === 3) {
+          x = new Date(x.innerHTML);
+          y = new Date(y.innerHTML);
+        } else {
+          x = x.innerHTML;
+          y = y.innerHTML;
+        }
+  
+        // Сравниваем значения и меняем их местами при необходимости
+        if (x > y) {
+          shouldSwitch = true;
+          break;
+        }
+      }
+      
+      if (shouldSwitch) {
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+      }
+    }
+  }
+
+const ezhednevnik = document.querySelector('.firstBlock');
+var lastClickedRow = null;
+
+ezhednevnik.addEventListener("click", function(event){
+  var clickedRow = event.target.closest('tbody tr');
+
+  for (var i = document.querySelector('.secondBlock').children.length - 1; i >= 0; i--) {
+    document.querySelector('.secondBlock').children[i].classList.remove('activeDiscription');
+}
+
+  if (clickedRow) {
+    if (clickedRow === lastClickedRow) {
+      // Повторное нажатие на строку
+      document.querySelector('.firstBlock').style.width = "100%";
+      document.querySelector('.secondBlock').style.display = "none";
+      lastClickedRow = null;
+      } 
+    else {
+      // event.target.closest('button').classList.add('activeButton');
+      document.querySelector('.firstBlock').style.width = "70%";
+      document.querySelector('.secondBlock').style.display = "block";
+      document.getElementById(event.target.closest('tbody tr').getAttribute('data-target')).classList.add('activeDiscription');
+      lastClickedRow = clickedRow;
+    }
+  }
 })
